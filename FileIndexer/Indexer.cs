3d1686319af59563b853508239ece90d3e4b6ee0
Adiq <Adiq94@gmail.com>
2016-07-29 00:16:57 +0200

    Init

---
+++ FileIndexer/Indexer.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Xml.Schema;
+
+namespace FileIndexer {
+    public class Indexer {
+        private static readonly string _baseDir = AppDomain.CurrentDomain.BaseDirectory;
+        private const string _filename = "test.txt";
+        private const string _indexFile = "test-index.txt";
+
+        private static readonly string _filePath = Path.Combine(_baseDir, _filename);
+        private static readonly string _indexPath = Path.Combine(_baseDir, _indexFile);
+
+        private static readonly Random _random = new Random();
+        private static readonly StringBuilder _builder = new StringBuilder();
+
+        public static void GenerateFile(Megabyte fileSize) {
+            if (File.Exists(_filePath)) {
+                File.Delete(_filePath);
+            }
+            using (StreamWriter sw = File.CreateText(_filePath)) {
+                long size = 0;
+                while (size <= fileSize.Value) {
+                    var length = _random.Next(1, 200);
+                    for (var i = 0; i < length; i++) {
+                        var ch = _random.Next(32, 126);
+                        _builder.Append((char)ch);
+                    }
+                    var line = _builder.ToString();
+                    size += Encoding.UTF8.GetByteCount(line);
+                    sw.WriteLine(line);
+                    _builder.Clear();
+                }
+
+            }
+
+        }
+
+        public static void GenerateIndexFile() {
+            var baseDir = AppDomain.CurrentDomain.BaseDirectory;
+            var filePath = Path.Combine(baseDir, _indexFile);
+            if (File.Exists(filePath)) {
+                File.Delete(filePath);
+            }
+
+            using (StreamReader sr = File.OpenText(_filePath))
+            using (StreamWriter sw = File.CreateText(_indexPath)) {
+                string line;
+                long size = 0;
+                long lineNumber = 0;
+                while ((line = sr.ReadLine()) != null) {
+                    size += Encoding.UTF8.GetByteCount(line + Environment.NewLine);
+                    var indexEntry = $"{lineNumber} {size}";
+                    sw.WriteLine(indexEntry);
+                    lineNumber++;
+                }
+
+            }
+        }
+
+        public static IDictionary<long, long> CreateDictionaryFromIndex() {
+            var dic = new Dictionary<long, long>();
+
+            using (StreamReader sr = File.OpenText(_indexPath)) {
+                string line;
+                while ((line = sr.ReadLine()) != null) {
+                    var splitted = line.Split(' ');
+                    dic.Add(long.Parse(splitted[0]), long.Parse(splitted[1]));
+                }
+
+            }
+            return dic;
+        }
+
+        public static string GetLineByLinearSearch(long lineNumber) {
+            using (StreamReader sr = File.OpenText(_filePath)) {
+                for (var i = 0l; i < lineNumber - 1; i++) {
+                    sr.ReadLine();
+                }
+                return sr.ReadLine();
+            }
+        }
+
+        public static string GetLineByDictionaryLookup(IDictionary<long,long> index, long lineNumber) {
+            using (StreamReader sr = File.OpenText(_filePath)) {
+                var offset = index[lineNumber - 2];
+                sr.BaseStream.Seek(offset, SeekOrigin.Begin);
+                return sr.ReadLine();
+            }
+        }
+
+        public static long CountLines() {
+            using (StreamReader sr = File.OpenText(_filePath)) {
+                var length = 0l;
+                while ((sr.ReadLine()) != null) {
+                    length++;
+                }
+                return length;
+            }
+        }
+
+    }
+
+
+    public class Megabyte {
+        private readonly long _value;
+
+        public long Value {
+            get { return _value * 1024 * 1024; }
+        }
+
+        public Megabyte(long value) {
+            _value = value;
+        }
+    }
+}
--- FileIndexer/Program.cs
+++ FileIndexer/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,6 +8,45 @@ using System.Threading.Tasks;
 namespace FileIndexer {
     class Program {
         static void Main(string[] args) {
+            Indexer.GenerateFile(new Megabyte(1000));
+            Indexer.GenerateIndexFile();
+            var index = Indexer.CreateDictionaryFromIndex();
+            var lineCount = Indexer.CountLines();
+            Console.WriteLine($"Line count: {lineCount}");
+            CompareSearchTime(index, 5);
+            CompareSearchTime(index, 500);
+            CompareSearchTime(index, 50000);
+            CompareSearchTime(index, 250000);
+
+            CompareSearchTime(index, lineCount / 8);
+            CompareSearchTime(index, lineCount / 4);
+            CompareSearchTime(index, lineCount / 2);
+            CompareSearchTime(index, lineCount);
+
+            Console.Read();
+        }
+
+        private static void CompareSearchTime(IDictionary<long, long> index, long lineNumber) {
+            Console.WriteLine($"Line number: {lineNumber}");
+            var sw = new Stopwatch();
+
+            sw.Start();
+            var line = Indexer.GetLineByLinearSearch(lineNumber);
+            sw.Stop();
+
+            Console.WriteLine(line);
+            Console.WriteLine($"Linear time(ms): {sw.ElapsedMilliseconds}");
+            sw.Reset();
+
+            sw.Start();
+            line = Indexer.GetLineByDictionaryLookup(index, lineNumber);
+            sw.Stop();
+
+            Console.WriteLine(line);
+            Console.WriteLine($"Lookup time(ms): {sw.ElapsedMilliseconds}");
+
+            Console.WriteLine();
+            Console.WriteLine();
         }
     }
 }
